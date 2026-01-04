57b063f33c03e59a87e845cb3fb74403983daf6c
JuanMalaver1511 <juanmalaver1511@gmail.com>
2026-01-03 19:56:12 -0500

    Actualizar contenido de la sección Hero y agregar descripciones a los servicios en la página de inicio

--- frontend/src/app/components/inicio/inicio.ts
+++ frontend/src/app/components/inicio/inicio.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [Navbar],
+  imports: [Navbar, RouterLink],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
