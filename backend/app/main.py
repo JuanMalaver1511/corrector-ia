cb7ad5f9f826332084db753272827e8bf15bd933
Juan Manuel Malaver <96750905+JuanMalaver1511@users.noreply.github.com>
2026-01-27 13:46:20 -0500

    Fix import path for routers in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import upload, ia
+from routers import upload, ia
 from dotenv import load_dotenv
 
 
